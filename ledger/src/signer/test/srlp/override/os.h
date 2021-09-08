bb11afb03024c670de4f3f0360903da1399b733f
Lucas Gabriel Vuotto <lucas@iovlabs.org>
2021-09-08 21:31:08 +0000

    Run project's unit test in CI
    - Use `rm -f` in `clean` target, to avoid erroring if any of the files
      doesn't exist, like when running `make clean` twice in a row.
    - Add a `test` target for Makefiles in signer's test directories
    - Use TCPSigner's os.h in Signer's srlp tests
    - middleware/term: don't require a TTY if running on CI

---
+++ ledger/src/signer/test/srlp/override/os.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *   HSM 2.1
+ *   (c) 2021 RSK
+ *   Ledger Nano S BOLOS simulator layer
+ ********************************************************************************/
+
+#ifndef __SIMULATOR_OS
+#define __SIMULATOR_OS
+
+#include "os_hash.h"
+#include "os_attestation.h"
+#include "os_ecdsa.h"
+#include "os_exceptions.h"
+#include "os_io.h"
+
+/*#include "hsmsim_explicit_bzero.h"*/
+
+/**
+ * Miscellaneous 
+ */
+#define PIC(x) (x)
+
+void os_memmove(void *dst, const void *src, unsigned int length);
+
+unsigned int os_perso_isonboarded();
+
+void nvm_write(void *dst_adr, void *src_adr, unsigned int src_len);
+
+#endif // __SIMULATOR_OS
---
+++ ledger/src/signer/test/srlp/override/os_hash.h
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ *   HSM 2.1
+ *   (c) 2021 RSK
+ *   Ledger Nano S BOLOS simulator layer
+ * 
+ *   Hashing functions
+ ********************************************************************************/
+
+#ifndef __SIMULATOR_OS_HASHING
+#define __SIMULATOR_OS_HASHING
+
+#include "sha256.h"
+#include "keccak256.h"
+
+typedef enum {
+    CX_ALGO_SHA256 = 0x01,
+    CX_ALGO_KECCAK256
+} cx_algo_t;
+
+typedef struct cx_hash_header_s {
+    cx_algo_t algo;
+} cx_hash_t;
+
+typedef struct cx_sha256_s {
+    cx_hash_t header;
+    SHA256_CTX ctx;
+} cx_sha256_t;
+
+typedef struct cx_sha3_s {
+    cx_hash_t header;
+    SHA3_CTX ctx;
+} cx_sha3_t;
+
+int cx_sha256_init(cx_sha256_t *hash);
+
+int cx_keccak_init(cx_sha3_t *hash, int size);
+
+int cx_hash(cx_hash_t *hash, int mode, unsigned char *in,
+            unsigned int len, unsigned char *out);
+
+#endif // __SIMULATOR_OS_HASHING
