357e06460efc21c853babbbf5bd80aea90fa0d2a
italo sampaio <italo.sampaio@iovlabs.org>
2022-12-19 10:32:47 -0300

    Break bootloader main loop into 2 steps
    Split bootloader main into APDU processing and exception processing to make this module testable

--- ledger/src/ui/src/bootloader.c
+++ ledger/src/ui/src/bootloader.c
@@ -86,12 +86,134 @@ static void run_signer_app(void) {
     }
 }
 
+unsigned int bootloader_process_apdu(
+    volatile unsigned int rx,
+    bootloader_mode_t mode,
+    volatile unsigned char *onboard_performed) {
+    unsigned int tx = 0;
+
+    // no apdu received, well, reset the session, and reset the
+    // bootloader configuration
+    if (rx == 0) {
+        THROW(ERR_EMPTY_BUFFER);
+    }
+
+    if (APDU_CLA() != CLA) {
+        THROW(ERR_INVALID_CLA);
+    }
+
+    // We don't accept any command after onboard is performed in
+    // onboard mode, the user is required to unplug the device
+    // before proceeding
+    if (mode == BOOTLOADER_MODE_ONBOARD && *onboard_performed) {
+        THROW(ERR_INS_NOT_SUPPORTED);
+    }
+
+    // unauthenticated instruction
+    switch (APDU_CMD()) {
+    case RSK_SEED_CMD: // Send wordlist
+        reset_if_starting(RSK_META_CMD_UIOP);
+        tx = set_host_seed(rx, &onboard_ctx);
+        break;
+    case RSK_PIN_CMD: // Send pin_buffer
+        reset_if_starting(RSK_META_CMD_UIOP);
+        tx = update_pin_buffer(rx);
+        break;
+    case RSK_IS_ONBOARD: // Wheter it's onboarded or not
+        reset_if_starting(RSK_IS_ONBOARD);
+        tx = is_onboarded();
+        break;
+    case RSK_WIPE: //--- wipe and onboard device ---
+        reset_if_starting(RSK_META_CMD_UIOP);
+        tx = onboard_device(&onboard_ctx);
+        clear_pin();
+        if (mode == BOOTLOADER_MODE_ONBOARD) {
+            *onboard_performed = 1;
+        }
+        break;
+    case RSK_NEWPIN:
+        reset_if_starting(RSK_META_CMD_UIOP);
+        tx = set_pin();
+        clear_pin();
+        break;
+    case RSK_ECHO_CMD: // echo
+        reset_if_starting(RSK_ECHO_CMD);
+        tx = echo(rx);
+        break;
+    case RSK_MODE_CMD: // print mode
+        reset_if_starting(RSK_MODE_CMD);
+        tx = get_mode();
+        break;
+    case INS_ATTESTATION:
+        reset_if_starting(INS_ATTESTATION);
+        tx = get_attestation(rx, &attestation_ctx);
+        break;
+    case INS_SIGNER_AUTHORIZATION:
+        reset_if_starting(INS_SIGNER_AUTHORIZATION);
+        tx = do_authorize_signer(rx, &sigaut_ctx);
+        break;
+    case RSK_RETRIES:
+        reset_if_starting(RSK_RETRIES);
+        tx = get_retries();
+        break;
+    case RSK_UNLOCK_CMD: // Unlock
+        reset_if_starting(RSK_META_CMD_UIOP);
+        tx = unlock();
+        // The pin value will also be used in
+        // BOLOS_UX_CONSENT_APP_ADD command, so we can't wipe the
+        // pin buffer here
+        break;
+    case RSK_END_CMD: // return to dashboard
+        reset_if_starting(RSK_END_CMD);
+        autoexec = 1;
+        THROW(EX_BOOTLOADER_RSK_END);
+    case RSK_END_CMD_NOSIG: // return to dashboard
+        reset_if_starting(RSK_END_CMD_NOSIG);
+        autoexec = 0;
+        THROW(EX_BOOTLOADER_RSK_END);
+    default:
+        THROW(ERR_INS_NOT_SUPPORTED);
+        break;
+    }
+
+    return tx;
+}
+
+unsigned int bootloader_process_exception(unsigned short ex, unsigned int tx) {
+    unsigned short sw = 0;
+
+    // Reset the state in case of an error
+    if (ex != APDU_OK) {
+        reset_if_starting(0);
+    }
+
+    switch (ex & 0xF000) {
+    case 0x6000:
+    case 0x9000:
+        sw = ex;
+        break;
+    default:
+        sw = 0x6800 | (ex & 0x7FF);
+        break;
+    }
+
+    // Unexpected exception => report
+    // (check for a potential overflow first)
+    if (tx + 2 > sizeof(G_io_apdu_buffer)) {
+        tx = 0;
+        sw = 0x6983;
+    }
+    SET_APDU_AT(tx++, sw >> 8);
+    SET_APDU_AT(tx++, sw);
+
+    return tx;
+}
+
 // Main function for the bootloader. If mode == BOOTLOADER_MODE_ONBOARD,
 // commands are not accepted after the onboard is performed.
 static void bootloader_main(bootloader_mode_t mode) {
     volatile unsigned int rx = 0;
     volatile unsigned int tx = 0;
-    volatile unsigned int flags = 0;
     volatile unsigned char onboard_performed = 0;
 
     // Initialize current operation
@@ -107,135 +229,21 @@ static void bootloader_main(bootloader_mode_t mode) {
     // switch event, before the apdu is replied to the bootloader. This avoid
     // APDU injection faults.
     for (;;) {
-        volatile unsigned short sw = 0;
-
         BEGIN_TRY {
             TRY {
                 rx = tx;
                 tx = 0; // ensure no race in catch_other if io_exchange throws
                         // an error
-                rx = io_exchange(CHANNEL_APDU | flags, rx);
-                flags = 0;
+                rx = io_exchange(CHANNEL_APDU, rx);
 
-                // no apdu received, well, reset the session, and reset the
-                // bootloader configuration
-                if (rx == 0) {
-                    THROW(ERR_EMPTY_BUFFER);
-                }
-
-                if (APDU_CLA() != CLA) {
-                    THROW(ERR_INVALID_CLA);
-                }
-
-                // We don't accept any command after onboard is performed in
-                // onboard mode, the user is required to unplug the device
-                // before proceeding
-                if (mode == BOOTLOADER_MODE_ONBOARD && onboard_performed) {
-                    THROW(ERR_INS_NOT_SUPPORTED);
-                }
-
-                // unauthenticated instruction
-                switch (APDU_CMD()) {
-                case RSK_SEED_CMD: // Send wordlist
-                    reset_if_starting(RSK_META_CMD_UIOP);
-                    tx = set_host_seed(rx, &onboard_ctx);
-                    THROW(APDU_OK);
-                    break;
-                case RSK_PIN_CMD: // Send pin_buffer
-                    reset_if_starting(RSK_META_CMD_UIOP);
-                    tx = update_pin_buffer(rx);
-                    THROW(APDU_OK);
-                    break;
-                case RSK_IS_ONBOARD: // Wheter it's onboarded or not
-                    reset_if_starting(RSK_IS_ONBOARD);
-                    tx = is_onboarded();
-                    THROW(APDU_OK);
-                    break;
-                case RSK_WIPE: //--- wipe and onboard device ---
-                    reset_if_starting(RSK_META_CMD_UIOP);
-                    tx = onboard_device(&onboard_ctx);
-                    clear_pin();
-                    if (mode == BOOTLOADER_MODE_ONBOARD) {
-                        onboard_performed = 1;
-                    }
-                    THROW(APDU_OK);
-                    break;
-                case RSK_NEWPIN:
-                    reset_if_starting(RSK_META_CMD_UIOP);
-                    tx = set_pin();
-                    clear_pin();
-                    THROW(APDU_OK);
-                    break;
-                case RSK_ECHO_CMD: // echo
-                    reset_if_starting(RSK_ECHO_CMD);
-                    tx = echo(rx);
-                    THROW(APDU_OK);
-                    break;
-                case RSK_MODE_CMD: // print mode
-                    reset_if_starting(RSK_MODE_CMD);
-                    tx = get_mode();
-                    THROW(APDU_OK);
-                    break;
-                case INS_ATTESTATION:
-                    reset_if_starting(INS_ATTESTATION);
-                    tx = get_attestation(rx, &attestation_ctx);
-                    THROW(APDU_OK);
-                    break;
-                case INS_SIGNER_AUTHORIZATION:
-                    reset_if_starting(INS_SIGNER_AUTHORIZATION);
-                    tx = do_authorize_signer(rx, &sigaut_ctx);
-                    THROW(APDU_OK);
-                    break;
-                case RSK_RETRIES:
-                    reset_if_starting(RSK_RETRIES);
-                    tx = get_retries();
-                    THROW(APDU_OK);
-                    break;
-                case RSK_UNLOCK_CMD: // Unlock
-                    reset_if_starting(RSK_META_CMD_UIOP);
-                    tx = unlock();
-                    // The pin value will also be used in
-                    // BOLOS_UX_CONSENT_APP_ADD command, so we can't wipe the
-                    // pin buffer here
-                    THROW(APDU_OK);
-                    break;
-                case RSK_END_CMD: // return to dashboard
-                    reset_if_starting(RSK_END_CMD);
-                    autoexec = 1;
-                    return;
-                case RSK_END_CMD_NOSIG: // return to dashboard
-                    reset_if_starting(RSK_END_CMD_NOSIG);
-                    autoexec = 0;
-                    return;
-                default:
-                    THROW(ERR_INS_NOT_SUPPORTED);
-                    break;
-                }
+                tx = bootloader_process_apdu(rx, mode, &onboard_performed);
+                THROW(APDU_OK);
+            }
+            CATCH(EX_BOOTLOADER_RSK_END) {
+                break;
             }
             CATCH_OTHER(e) {
-                // Reset the state in case of an error
-                if (e != APDU_OK) {
-                    reset_if_starting(0);
-                }
-
-                switch (e & 0xF000) {
-                case 0x6000:
-                case 0x9000:
-                    sw = e;
-                    break;
-                default:
-                    sw = 0x6800 | (e & 0x7FF);
-                    break;
-                }
-
-                // Unexpected exception => report
-                // (check for a potential overflow first)
-                if (tx + 2 > sizeof(G_io_apdu_buffer)) {
-                    tx = 0;
-                    sw = 0x6983;
-                }
-                SET_APDU_AT(tx++, sw >> 8);
-                SET_APDU_AT(tx++, sw);
+                tx = bootloader_process_exception(e, tx);
             }
             FINALLY {
             }
