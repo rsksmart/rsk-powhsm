113c30e33df524c1614c5413d3279ab7c6de7285
Italo Sampaio <italo.sampaio@iovlabs.org>
2024-08-19 14:17:32 -0300

    Factored out signer UX logic to specific files
    The next commit will introduce some unit tests for the UX logic. This commit factors out that logic to make it testable.

--- firmware/src/ledger/signer/src/main.c
+++ firmware/src/ledger/signer/src/main.c
@@ -43,83 +43,20 @@
 #include "os_io_seproxyhal.h"
 
 #include "hsm.h"
+#include "signer_ux.h"
 
 // HAL includes
 #include "hal/communication.h"
 
-unsigned char G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
-
-// UI currently displayed
-enum UI_STATE { UI_INFO, UI_SCREENSAVER };
-enum UI_STATE uiState;
-ux_state_t ux;
-
-#define SCREEN_SAVER_TIMEOUT_MS 30000
 // The interval between two subsequent ticker events in milliseconds. This is
 // assumed to be 100ms according to the nanos-secure-sdk documentation.
 #define TICKER_INTERVAL_MS 100
-// Time spent in idle state. This timer is reset when a button is pressed.
-static unsigned int G_idle_time_ms;
+// The time in milliseconds after which the screen saver is displayed if no
+// button is pressed
+#define SCREEN_SAVER_TIMEOUT_MS 30000
 
-// clang-format off
-static const bagl_element_t bagl_ui_info_nanos[] = {
-    {
-        {BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000,
-         0xFFFFFF, 0, 0},
-        NULL,
-        0,
-        0,
-        0,
-        NULL,
-        NULL,
-        NULL,
-    },
-    {
-        {BAGL_LABELINE, 0x02, 0, 12, 128, 11, 0, 0, 0, 0xFFFFFF, 0x000000,
-         BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
-        "Signer running...",
-        0,
-        0,
-        0,
-        NULL,
-        NULL,
-        NULL,
-    }
-};
-
-static const bagl_element_t bagl_ui_screensaver_nanos[] = {
-    {
-        {BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000,
-         0x000000, 0, 0},
-        NULL,
-        0,
-        0,
-        0,
-        NULL,
-        NULL,
-        NULL,
-    },
-};
-// clang-format on
-
-static unsigned int bagl_ui_info_nanos_button(
-    unsigned int button_mask, unsigned int button_mask_counter) {
-    switch (button_mask) {
-    case BUTTON_EVT_RELEASED | BUTTON_LEFT:
-    case BUTTON_EVT_RELEASED | BUTTON_LEFT | BUTTON_RIGHT:
-        //     We removed this function, but leave it in src in case its needed
-        //     for debug. io_seproxyhal_touch_exit(NULL);
-        break;
-    }
-
-    return 0;
-}
-
-static unsigned int bagl_ui_screensaver_nanos_button(
-    unsigned int button_mask, unsigned int button_mask_counter) {
-    // no-op - button presses are handled directly in the event loop
-    return 0;
-}
+unsigned char G_io_seproxyhal_spi_buffer[IO_SEPROXYHAL_BUFFER_SIZE_B];
+ux_state_t ux;
 
 unsigned short io_exchange_al(unsigned char channel, unsigned short tx_len) {
     switch (channel & ~(IO_FLAGS)) {
@@ -151,46 +88,6 @@ void io_seproxyhal_display(const bagl_element_t *element) {
     io_seproxyhal_display_default((bagl_element_t *)element);
 }
 
-static void ui_info(void) {
-    uiState = UI_INFO;
-    UX_DISPLAY(bagl_ui_info_nanos, NULL);
-}
-
-static void ui_screensaver(void) {
-    uiState = UI_SCREENSAVER;
-    UX_DISPLAY(bagl_ui_screensaver_nanos, NULL);
-}
-
-static void handle_button_press(void) {
-    G_idle_time_ms = 0;
-}
-
-static void handle_ticker_event(void) {
-    unsigned int last_idle_time_ms = G_idle_time_ms;
-    G_idle_time_ms += TICKER_INTERVAL_MS;
-    // Handle overflow
-    if (G_idle_time_ms < last_idle_time_ms) {
-        G_idle_time_ms = last_idle_time_ms;
-    }
-}
-
-static void handle_ui_state(void) {
-    switch (uiState) {
-    case UI_INFO:
-        if (G_idle_time_ms >= SCREEN_SAVER_TIMEOUT_MS) {
-            ui_screensaver();
-        }
-        break;
-    case UI_SCREENSAVER:
-        if (G_idle_time_ms < SCREEN_SAVER_TIMEOUT_MS) {
-            ui_info();
-        }
-        break;
-    default:
-        break;
-    }
-}
-
 unsigned char io_event(unsigned char channel) {
     // nothing done with the event, throw an error on the transport layer if
     // needed
@@ -203,7 +100,7 @@ unsigned char io_event(unsigned char channel) {
 
     case SEPROXYHAL_TAG_BUTTON_PUSH_EVENT: // for Nano S
         UX_BUTTON_PUSH_EVENT(G_io_seproxyhal_spi_buffer);
-        handle_button_press();
+        signer_ux_handle_button_press();
         break;
 
     case SEPROXYHAL_TAG_DISPLAY_PROCESSED_EVENT:
@@ -214,9 +111,8 @@ unsigned char io_event(unsigned char channel) {
             // defaulty retrig very soon (will be overriden during
             // stepper_prepro)
             UX_CALLBACK_SET_INTERVAL(500);
-            handle_ui_state();
         });
-        handle_ticker_event();
+        signer_ux_handle_ticker_event(TICKER_INTERVAL_MS);
         break;
 
     // unknown events are acknowledged
@@ -286,8 +182,7 @@ __attribute__((section(".boot"))) int main(int argc, char **argv) {
             USB_power(0);
             USB_power(1);
 
-            ui_info();
-            G_idle_time_ms = 0;
+            signer_ux_init(SCREEN_SAVER_TIMEOUT_MS);
 
             // next timer callback in 500 ms
             UX_CALLBACK_SET_INTERVAL(500);
---
+++ firmware/src/ledger/signer/src/signer_ux_info.c
@@ -0,0 +1,63 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 RSK Labs Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "os_io_seproxyhal.h"
+#include "signer_ux.h"
+
+// clang-format off
+static const bagl_element_t bagl_ui_info_nanos[] = {
+    {
+        {BAGL_RECTANGLE, 0x00, 0, 0, 128, 32, 0, 0, BAGL_FILL, 0x000000,
+         0xFFFFFF, 0, 0},
+        NULL,
+        0,
+        0,
+        0,
+        NULL,
+        NULL,
+        NULL,
+    },
+    {
+        {BAGL_LABELINE, 0x02, 0, 12, 128, 11, 0, 0, 0, 0xFFFFFF, 0x000000,
+         BAGL_FONT_OPEN_SANS_REGULAR_11px | BAGL_FONT_ALIGNMENT_CENTER, 0},
+        "Signer running...",
+        0,
+        0,
+        0,
+        NULL,
+        NULL,
+        NULL,
+    }
+};
+// clang-format on
+
+static unsigned int bagl_ui_info_nanos_button(
+    unsigned int button_mask, unsigned int button_mask_counter) {
+    // no-op - button presses are handled directly in the event loop
+    return 0;
+}
+
+void signer_ux_info(void) {
+    UX_DISPLAY(bagl_ui_info_nanos, NULL);
+}
