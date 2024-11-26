4e2c779113160a3ccee5d819c31e3ad7ad90732b
Italo Sampaio <italo.sampaio@iovlabs.org>
2024-11-26 10:11:13 -0300

    Adds optional exec argument unit tests scripts
    Adds an optional `exec` argument to all run-all.sh unit test scripts
    that allows the user to just run the tests straight away, without launching
    a docker container. The default behavior is running the tests inside a
    `hsm:mware` docker container.
    
    Additionally, modifies the `gen-coverage` script to set the optional `exec`
    argument to the scripts, since the `gen-coverage` script already runs its
    main logic inside a `hsm:mware` docker image.

--- firmware/src/powhsm/test/run-all.sh
+++ firmware/src/powhsm/test/run-all.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-BASEDIR=$(dirname $0)
-TESTDIRS="btcscript btctx difficulty srlp svarint trie"
-TESTDIRS=${1:-"$TESTDIRS"}
 
-for d in $TESTDIRS; do
-    echo "******************************"
-    echo "Testing $d..."
-    echo "******************************"
-    cd "$BASEDIR/$d"
-    make clean test || exit $?
-    cd - > /dev/null
-done
+if [[ $1 == "exec" ]]; then
+    BASEDIR=$(realpath $(dirname $0))
+    TESTDIRS="btcscript btctx difficulty srlp svarint trie"
+    for d in $TESTDIRS; do
+        echo "******************************"
+        echo "Testing $d..."
+        echo "******************************"
+        cd "$BASEDIR/$d"
+        make clean test || exit $?
+        cd - > /dev/null
+    done
+    exit 0
+else
+    # Script directory
+    REPOROOT=$(realpath $(dirname $0)/../../../../)
+    SCRIPT=$(realpath $0 --relative-to=$REPOROOT)
+
+    $REPOROOT/docker/mware/do-notty-nousb /hsm2 "./$SCRIPT exec"
+fi
