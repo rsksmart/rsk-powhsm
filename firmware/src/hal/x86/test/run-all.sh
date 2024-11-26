fc5aab672b3ad238fd7f8dfde78cc20f6990ae53
Italo Sampaio <100376888+italo-sampaio@users.noreply.github.com>
2024-11-26 16:21:25 -0300

    Fixes coverage report for feature/sgx branch (#224)
    - Triggers the coverage workflow for pushes to master and feature/sgx branches
    - Adds optional exec argument unit tests scripts
    - Some additional fixes to unit tests

--- firmware/src/hal/x86/test/run-all.sh
+++ firmware/src/hal/x86/test/run-all.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-ROOTDIR=$(dirname $0)/../../../../..
-TESTDIR=$(realpath $(dirname $0) --relative-to $ROOTDIR)
-TESTDIRS="bip32 endian hmac_sha256 hmac_sha512 keccak256"
-TESTDIRS=${1:-"$TESTDIRS"}
 
-for d in $TESTDIRS; do
-    echo "******************************"
-    echo "Testing $d..."
-    echo "******************************"
-    $ROOTDIR/docker/mware/do-notty-nousb /hsm2/$TESTDIR/$d "make clean test" || exit $?
-done
+if [[ $1 == "exec" ]]; then
+    BASEDIR=$(realpath $(dirname $0))
+    TESTDIRS="bip32 endian hmac_sha256 hmac_sha512 keccak256"
+    for d in $TESTDIRS; do
+        echo "******************************"
+        echo "Testing $d..."
+        echo "******************************"
+        cd "$BASEDIR/$d"
+        make clean test || exit $?
+        cd - > /dev/null
+    done
+    exit 0
+else
+    # Script directory
+    REPOROOT=$(realpath $(dirname $0)/../../../../..)
+    SCRIPT=$(realpath $0 --relative-to=$REPOROOT)
+
+    $REPOROOT/docker/mware/do-notty-nousb /hsm2 "./$SCRIPT exec"
+fi
