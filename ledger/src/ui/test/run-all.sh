f2af33155808ee440f4fa05c920879c8347ee60a
Ariel Mendelzon <amendelzon@rsk.co>
2025-07-16 15:37:24 +0200

    Fixing build
    - Fixed middleware docker image build
    - Fixing outdated runner and actions on GH workflows
    - Running firmware unit tests within a mware docker container for consistency

--- ledger/src/ui/test/run-all.sh
+++ ledger/src/ui/test/run-all.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-BASEDIR=$(dirname $0)
-TESTDIRS="attestation signer_authorization communication onboard pin unlock bootloader ux_handlers ui_heartbeat"
-TESTDIRS=${1:-"$TESTDIRS"}
 
-for d in $TESTDIRS; do
-    echo "******************************"
-    echo "Testing $d..."
-    echo "******************************"
-    cd "$BASEDIR/$d"
-    make clean test || exit $?
-    cd - > /dev/null
-done
+if [[ $1 == "exec" ]]; then
+    BASEDIR=$(realpath $(dirname $0))
+    TESTDIRS="attestation signer_authorization communication onboard pin unlock bootloader ux_handlers ui_heartbeat"
+    for d in $TESTDIRS; do
+        echo "******************************"
+        echo "Testing $d..."
+        echo "******************************"
+        cd "$BASEDIR/$d"
+        make clean test || exit $?
+        cd - > /dev/null
+    done
+    exit 0
+else
+    # Script directory
+    REPOROOT=$(realpath $(dirname $0)/../../../../)
+    SCRIPT=$(realpath $0 --relative-to=$REPOROOT)
+
+    $REPOROOT/docker/mware/do-notty-nousb /hsm2 "./$SCRIPT exec"
+fi
