94a9ff940a7b7db705eee7cd3bbc09549bfe798e
Ariel Mendelzon <amendelzon@rsk.co>
2025-09-26 15:52:50 +0800

    Unit testing Ledger HAL's nvmem module

--- firmware/src/hal/ledger/test/run-all.sh
+++ firmware/src/hal/ledger/test/run-all.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "exec" ]]; then
     BASEDIR=$(realpath $(dirname $0))
-    TESTDIRS="endorsement hash"
+    TESTDIRS="endorsement hash nvmem"
     for d in $TESTDIRS; do
         echo "******************************"
         echo "Testing $d..."
