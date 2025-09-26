042c77689e9b14ff33799fc5c2ccac3eb5d5d713
Ariel Mendelzon <amendelzon@rsk.co>
2025-09-26 15:49:16 +0800

    Unit testing Ledger HAL's endorsement module
    - Added Ledger HAL's endorsement module unit tests
    - Added common artifacts for Ledger HAL's unit testing
    - Added Ledger HAL's test script to the CI
    - Removed no longer needed .gitkeep

---
+++ firmware/src/hal/ledger/test/common/mock.h
@@ -0,0 +1,37 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 RSK Labs Ltd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef __MOCK_H
+#define __MOCK_H
+
+unsigned int os_endorsement_key2_derive_sign_data(unsigned char *src,
+                                                  unsigned int srcLength,
+                                                  unsigned char *signature);
+
+unsigned int os_endorsement_get_code_hash(unsigned char *buffer);
+
+unsigned int os_endorsement_get_public_key(unsigned char index,
+                                           unsigned char *buffer);
+
+#endif // __MOCK_H
---
+++ firmware/src/hal/ledger/test/run-all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [[ $1 == "exec" ]]; then
+    BASEDIR=$(realpath $(dirname $0))
+    TESTDIRS="endorsement"
+    for d in $TESTDIRS; do
+        echo "******************************"
+        echo "Testing $d..."
+        echo "******************************"
+        cd "$BASEDIR/$d"
+        make clean test || exit $?
+        cd - > /dev/null
+    done
+    exit 0
+else
+    # Script directory
+    REPOROOT=$(realpath $(dirname $0)/../../../../..)
+    SCRIPT=$(realpath $0 --relative-to=$REPOROOT)
+
+    $REPOROOT/docker/mware/do-notty-nousb /hsm2 "./$SCRIPT exec"
+fi
