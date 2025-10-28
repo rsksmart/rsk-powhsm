711222a1563beca37218a03d4383d8883b3ddb7e
Italo Sampaio <100376888+italo-sampaio@users.noreply.github.com>
2025-10-27 22:29:33 -0300

    Added unit tests for powHSM hearbeat (#457)
    Added unit tests for firmware/src/powhsm/src/heartbeat.c

--- firmware/src/powhsm/test/run-all.sh
+++ firmware/src/powhsm/test/run-all.sh
@@ -2,7 +2,7 @@
 
 if [[ $1 == "exec" ]]; then
     BASEDIR=$(realpath $(dirname $0))
-    TESTDIRS="btcscript btctx difficulty srlp svarint trie"
+    TESTDIRS="btcscript btctx difficulty heartbeat srlp svarint trie"
     for d in $TESTDIRS; do
         echo "******************************"
         echo "Testing $d..."
