2633179665316bb70528cc83e07f8169a3c55ac9
Ariel Mendelzon <amendelzon@rsk.co>
2025-12-24 14:29:00 +0700

    Updated middleware unit tests after legacy signing logic removal

--- middleware/tests/comm/test_bitcoin.py
+++ middleware/tests/comm/test_bitcoin.py
@@ -22,20 +22,61 @@
 
 from unittest import TestCase
 from comm.bitcoin import (
-    get_tx_hash_for_unsigned_tx,
     get_signature_hash_for_p2sh_input,
+    get_signature_hash_for_p2sh_p2wsh_input,
     get_block_hash_as_int,
     get_merkle_root,
-    get_unsigned_tx,
-    get_tx_hash,
-    get_tx_version,
 )
 
 import logging
 
 logging.disable(logging.CRITICAL)
 
-SAMPLE_1_UNSIGNED = """
+# Taken from https://en.bitcoin.it/wiki/BIP_0143#P2SH-P2WSH
+SAMPLE_1 = """
+010000000136641869ca081e70f394c6948e8af409e18b619df2ed74aa106c1ca29787b96e010000
+0000ffffffff0200e9a435000000001976a914389ffce9cd9ae88dcc0631e88a821ffdbe9bfe2688
+acc0832f05000000001976a9147480a33f950689af511e6e84c138dbbd3c3ee41588ac00000000
+""".replace("\n", "").replace("\r", "")
+
+SAMPLE_1_SIGHASH_I0 = "185c0be5263dce5b4bb50a047973c1b6272bfbd0103a89444597dc40b248ee7c"
+
+SAMPLE_1_WITNESS_SCRIPT = """
+56210307b8ae49ac90a048e9b53357a2354b3334e9c8bee813ecb98e99a7e07e8c3ba32103b28f0c28bfab54554ae8c658ac5c3e0ce6e79ad336331f78c428dd43eea8449b21034b8113d703413d57761b8b9781957b8c0ac1dfe69f492580ca4195f50376ba4a21033400f6afecb833092a9a21cfdf1ed1376e58c5d1f47de74683123987e967a8f42103a6d48b1131e94ba04d9737d61acdaa1322008af9602b3b14862c07a1789aac162102d8b661b0b3302ee2f162b09e07a55ad5dfbe673a9f01d9f0c19617681024306b56ae
+""".replace("\n", "").replace("\r", "")
+SAMPLE_1_I0_OUTVAL = 987654321
+
+SAMPLE_2 = """
+02000000018fd94eaa9b2c5baf1356826d549741ea53d7063e830e9907cf86cdd4fe46c278010000
+00232200200fca68baeb0517ceaf99d1d61947d142c29fbba23b6c173d18b034992df2e1c2ffffff
+ff07fc240900000000001976a914ac3df06ee0d2596ed38f23722335f4c01ecdc40288ac9c028547
+000000001976a91473179e1c334c7b9c393f3a31d6e9c1896bf480cd88ac5c3d2c00000000001976
+a91487b7bfba7a7703864dec1a1aeb0536d4c09120d488ac256c0800000000001976a91435ad11a2
+ac31cfbc01426351c8ba4c333f90296088ac1c302900000000001976a91487b7bfba7a7703864dec
+1a1aeb0536d4c09120d488ac347f8f05000000001976a914f4ef22351609132290bf20f8bca034c6
+ee101f5888acaedb7dc63100000017a914cb24588cf67c2126c63fa55bedd02dff319626c6870000
+0000
+""".replace("\n", "").replace("\r", "")
+
+SAMPLE_2_SIGHASH_I0 = "a93253994b04069f720b116fc1ba161839a67b00cccc9af52e2fa76569e27da2"
+
+SAMPLE_2_WITNESS_SCRIPT = """
+6455210274c7a2c584eb9dbc3d78fede48d1258b974cd537d82fce4d18f0e85604774e652102daa3
+cd74c9d06bc7928e1214f0de4ee5744c694975d70e160f562c5e1c4f587d2102f0844d9c1df50f55
+97118659badf40ea6956903f405f6a604402c8014d926fd42103250c11be0561b1d7ae168b1f59e3
+9cbc1fd1ba3cf4d2140c1a365b2723a2bf93210328f0386fe2b2840a424d3a5073be89a4cb643c43
+4752b8c865e867611d0041832103b8faf3ebcf3fe89085fe8c6046dd9cece45bb56e466103f96b4f
+ca2f21072ded2103e59ed1ba1240ac6ff0cb9234afc0e44d6e7653219982d4f1eb55844395e93496
+2103e8f68168606392c5e17f2f9bf1fd3fa55c295cad3990e72e7e3e4470ed644c8f2103f4501687
+1f91a1d81984280b9abd1aad6987e1a73a51e0ae8c920b7b238d62cb59ae670350cd00b275532102
+370a9838e4d15708ad14a104ee5606b36caaaaf739d833e67770ce9fd9b3ec80210257c293086c4d
+4fe8943deda5f890a37d11bebd140e220faa76258a41d077b4d42103c2660a46aa73078ee6016dee
+953488566426cf55fc8011edd0085634d75395f92103cd3e383ec6e12719a6c69515e5559bcbe037
+d0aa24c187e1e26ce932e22ad7b354ae68
+""".replace("\n", "").replace("\r", "")
+SAMPLE_2_I0_OUTVAL = 215083478191
+
+SAMPLE_3 = """
 010000000187ace6cb83436c876dd2233dfadf537068ecb1b9a0000d3590c9072d46d2bf4c000000006e0
 000004c69522102cd53fc53a07f211641a677d250f6de99caf620e8e77071e811a28b3bcddf0be1210362
 634ab57dae9cb373a5d536e66a8c4f67468bbcfb063809bab643072d78a1242103c5946b3fbae03a65423
@@ -44,25 +85,9 @@ b794549667efd57f083e018c1a4726c73ccb4388ac00301a1e0100000017a914896ed9f3446d51b5
 f0b6ef81b2bde55140e8700000000
 """.replace("\n", "").replace("\r", "")
 
-SAMPLE_1_PARTIALLY_SIGNED = """
-010000000187ace6cb83436c876dd2233dfadf537068ecb1b9a0000d3590c9072d46d2bf4c00000000b60
-048304502210084ddb5c7c6b57405c4068fa6634c8dd736d53ebf1ea878ad962026eecb86b57502206988
-e345a0af65ca5a72418580896028cdd1834404fe97774234725e494a18f101004c69522102cd53fc53a07
-f211641a677d250f6de99caf620e8e77071e811a28b3bcddf0be1210362634ab57dae9cb373a5d536e66a
-8c4f67468bbcfb063809bab643072d78a1242103c5946b3fbae03a654237da863c9ed534e0878657175b1
-32b8ca630f245df04db53aeffffffff02b030eb0b000000001976a914f6b794549667efd57f083e018c1a
-4726c73ccb4388ac00301a1e0100000017a914896ed9f3446d51b5510f7f0b6ef81b2bde55140e8700000
-000
-""".replace("\n", "").replace("\r", "")
+SAMPLE_3_SIGHASH_I0 = "db4ca3f81a68996e7c51e7e138524a53ab9770410f628199dc6ab5a5bac73e5c"
 
-SAMPLE_1_PARTIALLY_SIGNED_HASH = (
-    "e9ceb9260f258f13a029ad212f08719d8656b564cf4108b86199d83969de7c2e")
-SAMPLE_1_UNSIGNED_HASH = (
-    "f7e2b314eb12bd13481c5325ae3839fdbe5c508dbc7e24a44aac5e9992d07718")
-SAMPLE_1_SIGHASH_INPUT0 = (
-    "db4ca3f81a68996e7c51e7e138524a53ab9770410f628199dc6ab5a5bac73e5c")
-
-SAMPLE_2_UNSIGNED = """
+SAMPLE_4 = """
 01000000025cc070156cd1487db3c0c0d22affe09f208950bbe1bd6d2bc4a7c65749cee0c1000000006e000000
 4c69522102cd53fc53a07f211641a677d250f6de99caf620e8e77071e811a28b3bcddf0be1210362634ab57dae
 9cb373a5d536e66a8c4f67468bbcfb063809bab643072d78a1242103c5946b3fbae03a654237da863c9ed534e0
@@ -74,130 +99,51 @@ SAMPLE_2_UNSIGNED = """
 b5510f7f0b6ef81b2bde55140e8700000000
 """.replace("\n", "").replace("\r", "")
 
-SAMPLE_2_PARTIALLY_SIGNED = """
-01000000025cc070156cd1487db3c0c0d22affe09f208950bbe1bd6d2bc4a7c65749cee0c100000000b5004730
-4402203743f5bc4632b46fa261de4bdeaa1b3d617bd9dcaaf9fb3f9272c95c7f7b8b11022055464b2dae639037
-b42b3bc559bcf7580a7aa3e421c1511a6419ae7338029cc601004c69522102cd53fc53a07f211641a677d250f6
-de99caf620e8e77071e811a28b3bcddf0be1210362634ab57dae9cb373a5d536e66a8c4f67468bbcfb063809ba
-b643072d78a1242103c5946b3fbae03a654237da863c9ed534e0878657175b132b8ca630f245df04db53aeffff
-ffffbe6be3ddaf27e9609c9b319db9c97eae36e88dcda760ff4e0b77ccd086c7f4c600000000b5004730440220
-046b94168f1181a81ac8b4d7543b6392743d81a5b0c494db95be12ce2d9ba91002201dc5ad6c9f7b6171678a78
-a910fbd59150d8f86ef39f3244a82761cbf87670c801004c69522102cd53fc53a07f211641a677d250f6de99ca
-f620e8e77071e811a28b3bcddf0be1210362634ab57dae9cb373a5d536e66a8c4f67468bbcfb063809bab64307
-2d78a1242103c5946b3fbae03a654237da863c9ed534e0878657175b132b8ca630f245df04db53aeffffffff02
-1044d871020000001976a914f6b794549667efd57f083e018c1a4726c73ccb4388ac008d380c0100000017a914
-896ed9f3446d51b5510f7f0b6ef81b2bde55140e8700000000
-""".replace("\n", "").replace("\r", "")
+SAMPLE_4_SIGHASH_I0 = "ef95325f9c819476a54097abe466374f8293f26637af3686833d73dc29be2093"
+SAMPLE_4_SIGHASH_I1 = "20d8b8e413868c08e987ae11c7280b95293c05f0e191ef847a6060593b759814"
 
-SAMPLE_2_PARTIALLY_SIGNED_HASH = (
-    "39ae0553e91565866acc81a61da52cd87702ed2b4632f52bba19e3d9ba7e46ce")
-SAMPLE_2_UNSIGNED_HASH = (
-    "f8318d6c071efd988411cd63ee7d3fbf736d5ed38ba0679fa0be7acddb1bddda")
-SAMPLE_2_SIGHASH_INPUT0 = (
-    "ef95325f9c819476a54097abe466374f8293f26637af3686833d73dc29be2093")
-SAMPLE_2_SIGHASH_INPUT1 = (
-    "20d8b8e413868c08e987ae11c7280b95293c05f0e191ef847a6060593b759814")
-
-SAMPLE_3_UNSIGNED = """
-0100000001ec7281cfc9e59db5c1e69fc294846572bd3d78091bd0c6fdc21c2209c500556a000000004b000000
-475221031da807c71c2f303b7f409dd2605b297ac494a563be3b9ca5f52d95a43d183cc521036bb9eab797eadc
-8b697f0e82a01d01cabbfaaca37e5bafc06fdc6fdd38af894a52aeffffffff01b01c661b0c0000001976a914f7
-ee4511563688dce9b5d74e07cc9932b8e01f2288ac00000000
-""".replace("\n", "").replace("\r", "")
-
-SAMPLE_3_UNSIGNED_HASH = (
-    "ea73e8a9a6b98da8ba3f47d28f94af6818c8e906937a95f302263920b4cda792")
-SAMPLE_3_SIGHASH_INPUT0 = (
-    "5aeb5a6bbecda560b297da643559ce3c1b727076fb2ba1118c94d5906617aa19")
-
-SAMPLE_TX_V2 = """
-02000000010ac6236167ee3c88aee00f0f3b89f92b43a84797a71b6b62ffd354f11ef8c2f000000000de000000
-004cd8645221024c759affafc5589872d218ca30377e6d97211c039c375672c169ba76ce7fad6a21031f4aa494
-3fa2b731cd99c551d6992021555877b3b32c125385600fbc1b89c2a92103767a0994daa8babee7215b2371916d
-09fc1158de3c23feeefaae2dfe5baf483053670132b275522102132685d71b0109fecef0160f1efcab0187eff9
-16f4d472289741bff2666d0e1c2102ed498022f9d618a96f272b1990a640d9f24fb97d2648f8716f9ee22dc008
-eba721036f66639295ca8e4294c24d63e3fbc11247f6ba6a27b6b4de9a3492f414152d9b5368aeffffffff0294
-8e4b00000000001976a9140a4f09cbd39d5d8072b24385e1a9eb1c84ae544688acf83d6e0b0000000017a914ba
-053351893c7495e0c75d5abacb3ed886cf1ff88700000000
+SAMPLE_5 = """
+01000000024c62cd14e351b13b960e76502307cf26fc1dcf8681e4b0d9d1bb6ca69156d90f030000
+00220020a16b5755f7f6f96dbd65f5f0d6ab9418b89af4b1f14a1bb8a09062c35f0dcb54ffffffff
+fd55c4e8a3a616fedc40187a6214775006fdda1155544b6fd3486374ace05ce4050000002200200f
+bed83438cf3f837ab484b29426bb643fd36b1c800fb37076a3e9d50fbf3845ffffffff0200e9a435
+000000001976a914389ffce9cd9ae88dcc0631e88a821ffdbe9bfe2688acc0832f05000000001976
+a9147480a33f950689af511e6e84c138dbbd3c3ee41588ac00000000
 """.replace("\n", "").replace("\r", "")
 
 
 class TestBitcoin(TestCase):
-    def test_signed_unsigned_different_sample1(self):
-        self.assertNotEqual(SAMPLE_1_UNSIGNED, SAMPLE_1_PARTIALLY_SIGNED)
-
-    def test_get_tx_hash_for_unsigned_sample1(self):
-        self.assertEqual(SAMPLE_1_UNSIGNED_HASH,
-                         get_tx_hash_for_unsigned_tx(SAMPLE_1_UNSIGNED))
-
-    def test_get_tx_hash_sample1(self):
-        self.assertEqual(SAMPLE_1_UNSIGNED_HASH, get_tx_hash(SAMPLE_1_UNSIGNED))
-
-    def test_get_tx_hash_sample1_signed(self):
-        self.assertEqual(SAMPLE_1_PARTIALLY_SIGNED_HASH,
-                         get_tx_hash(SAMPLE_1_PARTIALLY_SIGNED))
-
-    def test_get_tx_for_unsigned_hash_partially_signed_sample1(self):
-        self.assertEqual(SAMPLE_1_UNSIGNED_HASH,
-                         get_tx_hash_for_unsigned_tx(SAMPLE_1_PARTIALLY_SIGNED))
-
-    def test_get_sighash_unsigned_sample1(self):
+    def test_get_sig_hash_sample_1_i0(self):
         self.assertEqual(
-            SAMPLE_1_SIGHASH_INPUT0,
-            get_signature_hash_for_p2sh_input(SAMPLE_1_UNSIGNED, 0),
-        )
+            SAMPLE_1_SIGHASH_I0,
+            get_signature_hash_for_p2sh_p2wsh_input(
+                SAMPLE_1, 0, SAMPLE_1_WITNESS_SCRIPT, SAMPLE_1_I0_OUTVAL))
 
-    def test_get_sighash_partially_signed_sample1(self):
+    def test_get_sig_hash_sample_2_i0(self):
         self.assertEqual(
-            SAMPLE_1_SIGHASH_INPUT0,
-            get_signature_hash_for_p2sh_input(SAMPLE_1_PARTIALLY_SIGNED, 0),
-        )
+            SAMPLE_2_SIGHASH_I0,
+            get_signature_hash_for_p2sh_p2wsh_input(
+                SAMPLE_2, 0, SAMPLE_2_WITNESS_SCRIPT, SAMPLE_2_I0_OUTVAL))
 
-    def test_get_unsigned_sample1(self):
-        self.assertEqual(SAMPLE_1_UNSIGNED, get_unsigned_tx(SAMPLE_1_PARTIALLY_SIGNED))
-
-    def test_get_unsigned_sample2(self):
-        self.assertEqual(SAMPLE_2_UNSIGNED, get_unsigned_tx(SAMPLE_2_PARTIALLY_SIGNED))
-
-    def test_signed_unsigned_different_sample2(self):
-        self.assertNotEqual(SAMPLE_2_UNSIGNED, SAMPLE_2_PARTIALLY_SIGNED)
-
-    def test_get_tx_hash_sample2(self):
-        self.assertEqual(SAMPLE_2_UNSIGNED_HASH, get_tx_hash(SAMPLE_2_UNSIGNED))
-
-    def test_get_tx_hash_sample2_signed(self):
-        self.assertEqual(SAMPLE_2_PARTIALLY_SIGNED_HASH,
-                         get_tx_hash(SAMPLE_2_PARTIALLY_SIGNED))
-
-    def test_get_tx_hash_for_unsigned_sample2(self):
-        self.assertEqual(SAMPLE_2_UNSIGNED_HASH,
-                         get_tx_hash_for_unsigned_tx(SAMPLE_2_UNSIGNED))
-
-    def test_get_tx_hash_for_unsigned_partially_signed_sample2(self):
-        self.assertEqual(SAMPLE_2_UNSIGNED_HASH,
-                         get_tx_hash_for_unsigned_tx(SAMPLE_2_PARTIALLY_SIGNED))
-
-    def test_get_sighash_unsigned_sample2_input0(self):
+    def test_get_sig_hash_sample_3_i0(self):
         self.assertEqual(
-            SAMPLE_2_SIGHASH_INPUT0,
-            get_signature_hash_for_p2sh_input(SAMPLE_2_UNSIGNED, 0),
-        )
+            SAMPLE_3_SIGHASH_I0,
+            get_signature_hash_for_p2sh_input(SAMPLE_3, 0))
 
-    def test_get_sighash_unsigned_sample2_input1(self):
+    def test_get_sig_hash_sample_4_i0(self):
         self.assertEqual(
-            SAMPLE_2_SIGHASH_INPUT1,
-            get_signature_hash_for_p2sh_input(SAMPLE_2_UNSIGNED, 1),
-        )
+            SAMPLE_4_SIGHASH_I0,
+            get_signature_hash_for_p2sh_input(SAMPLE_4, 0))
 
-    def test_get_tx_hash_not_signed_sample3(self):
-        self.assertEqual(SAMPLE_3_UNSIGNED_HASH,
-                         get_tx_hash_for_unsigned_tx(SAMPLE_3_UNSIGNED))
-
-    def test_get_sighash_unsigned_sample3_input0(self):
+    def test_get_sig_hash_sample_4_i1(self):
         self.assertEqual(
-            SAMPLE_3_SIGHASH_INPUT0,
-            get_signature_hash_for_p2sh_input(SAMPLE_3_UNSIGNED, 0),
-        )
+            SAMPLE_4_SIGHASH_I1,
+            get_signature_hash_for_p2sh_input(SAMPLE_4, 1))
+
+    def test_get_sig_hash_sample_5_i0(self):
+        with self.assertRaises(ValueError) as err:
+            get_signature_hash_for_p2sh_input(SAMPLE_5, 0)
+        self.assertIn("Invalid redeem script", str(err.exception))
 
     # Taken from https://www.blockchain.com/btc/block/624147
     # Raw taken from first 80 bytes of https://blockchain.info/block/624147?format=hex
@@ -260,9 +206,3 @@ class TestBitcoin(TestCase):
     def test_get_merkle_root_malformed(self):
         with self.assertRaises(ValueError):
             get_merkle_root("aabbcc")
-
-    def test_get_tx_version_a(self):
-        self.assertEqual(1, get_tx_version(SAMPLE_1_UNSIGNED))
-
-    def test_get_tx_version_b(self):
-        self.assertEqual(2, get_tx_version(SAMPLE_TX_V2))
