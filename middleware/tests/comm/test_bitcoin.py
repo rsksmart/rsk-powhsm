3ee6420149eada09fabed574166eb5c70a53019b
Italo Sampaio <italo.sampaio@iovlabs.org>
2026-04-22 18:37:40 -0300

    Restore get_tx_hash_for_unsigned_tx and its tests
    Brings back the 'get_tx_hash_for_unsigned_tx' helper, its tests and
    dependencies. These will be necessary for the next commit.

--- middleware/tests/comm/test_bitcoin.py
+++ middleware/tests/comm/test_bitcoin.py
@@ -26,6 +26,7 @@ from comm.bitcoin import (
     get_signature_hash_for_p2sh_p2wsh_input,
     get_block_hash_as_int,
     get_merkle_root,
+    get_tx_hash_for_unsigned_tx,
 )
 
 import logging
@@ -87,6 +88,20 @@ f0b6ef81b2bde55140e8700000000
 
 SAMPLE_3_SIGHASH_I0 = "db4ca3f81a68996e7c51e7e138524a53ab9770410f628199dc6ab5a5bac73e5c"
 
+SAMPLE_3_PARTIALLY_SIGNED = """
+010000000187ace6cb83436c876dd2233dfadf537068ecb1b9a0000d3590c9072d46d2bf4c00000000b60
+048304502210084ddb5c7c6b57405c4068fa6634c8dd736d53ebf1ea878ad962026eecb86b57502206988
+e345a0af65ca5a72418580896028cdd1834404fe97774234725e494a18f101004c69522102cd53fc53a07
+f211641a677d250f6de99caf620e8e77071e811a28b3bcddf0be1210362634ab57dae9cb373a5d536e66a
+8c4f67468bbcfb063809bab643072d78a1242103c5946b3fbae03a654237da863c9ed534e0878657175b1
+32b8ca630f245df04db53aeffffffff02b030eb0b000000001976a914f6b794549667efd57f083e018c1a
+4726c73ccb4388ac00301a1e0100000017a914896ed9f3446d51b5510f7f0b6ef81b2bde55140e8700000
+000
+""".replace("\n", "").replace("\r", "")
+
+SAMPLE_3_UNSIGNED_HASH = (
+    "f7e2b314eb12bd13481c5325ae3839fdbe5c508dbc7e24a44aac5e9992d07718")
+
 SAMPLE_4 = """
 01000000025cc070156cd1487db3c0c0d22affe09f208950bbe1bd6d2bc4a7c65749cee0c1000000006e000000
 4c69522102cd53fc53a07f211641a677d250f6de99caf620e8e77071e811a28b3bcddf0be1210362634ab57dae
@@ -101,6 +116,24 @@ b5510f7f0b6ef81b2bde55140e8700000000
 
 SAMPLE_4_SIGHASH_I0 = "ef95325f9c819476a54097abe466374f8293f26637af3686833d73dc29be2093"
 SAMPLE_4_SIGHASH_I1 = "20d8b8e413868c08e987ae11c7280b95293c05f0e191ef847a6060593b759814"
+
+SAMPLE_4_PARTIALLY_SIGNED = """
+01000000025cc070156cd1487db3c0c0d22affe09f208950bbe1bd6d2bc4a7c65749cee0c100000000b5004730
+4402203743f5bc4632b46fa261de4bdeaa1b3d617bd9dcaaf9fb3f9272c95c7f7b8b11022055464b2dae639037
+b42b3bc559bcf7580a7aa3e421c1511a6419ae7338029cc601004c69522102cd53fc53a07f211641a677d250f6
+de99caf620e8e77071e811a28b3bcddf0be1210362634ab57dae9cb373a5d536e66a8c4f67468bbcfb063809ba
+b643072d78a1242103c5946b3fbae03a654237da863c9ed534e0878657175b132b8ca630f245df04db53aeffff
+ffffbe6be3ddaf27e9609c9b319db9c97eae36e88dcda760ff4e0b77ccd086c7f4c600000000b5004730440220
+046b94168f1181a81ac8b4d7543b6392743d81a5b0c494db95be12ce2d9ba91002201dc5ad6c9f7b6171678a78
+a910fbd59150d8f86ef39f3244a82761cbf87670c801004c69522102cd53fc53a07f211641a677d250f6de99ca
+f620e8e77071e811a28b3bcddf0be1210362634ab57dae9cb373a5d536e66a8c4f67468bbcfb063809bab64307
+2d78a1242103c5946b3fbae03a654237da863c9ed534e0878657175b132b8ca630f245df04db53aeffffffff02
+1044d871020000001976a914f6b794549667efd57f083e018c1a4726c73ccb4388ac008d380c0100000017a914
+896ed9f3446d51b5510f7f0b6ef81b2bde55140e8700000000
+""".replace("\n", "").replace("\r", "")
+
+SAMPLE_4_UNSIGNED_HASH = (
+    "f8318d6c071efd988411cd63ee7d3fbf736d5ed38ba0679fa0be7acddb1bddda")
 
 SAMPLE_5 = """
 01000000024c62cd14e351b13b960e76502307cf26fc1dcf8681e4b0d9d1bb6ca69156d90f030000
@@ -144,6 +177,24 @@ class TestBitcoin(TestCase):
         with self.assertRaises(ValueError) as err:
             get_signature_hash_for_p2sh_input(SAMPLE_5, 0)
         self.assertIn("Invalid redeem script", str(err.exception))
+
+    def test_get_tx_hash_for_unsigned_sample_3(self):
+        self.assertEqual(SAMPLE_3_UNSIGNED_HASH,
+                         get_tx_hash_for_unsigned_tx(SAMPLE_3))
+
+    def test_get_tx_hash_for_unsigned_sample_3_partially_signed(self):
+        self.assertNotEqual(SAMPLE_3, SAMPLE_3_PARTIALLY_SIGNED)
+        self.assertEqual(SAMPLE_3_UNSIGNED_HASH,
+                         get_tx_hash_for_unsigned_tx(SAMPLE_3_PARTIALLY_SIGNED))
+
+    def test_get_tx_hash_for_unsigned_sample_4(self):
+        self.assertEqual(SAMPLE_4_UNSIGNED_HASH,
+                         get_tx_hash_for_unsigned_tx(SAMPLE_4))
+
+    def test_get_tx_hash_for_unsigned_sample_4_partially_signed(self):
+        self.assertNotEqual(SAMPLE_4, SAMPLE_4_PARTIALLY_SIGNED)
+        self.assertEqual(SAMPLE_4_UNSIGNED_HASH,
+                         get_tx_hash_for_unsigned_tx(SAMPLE_4_PARTIALLY_SIGNED))
 
     # Taken from https://www.blockchain.com/btc/block/624147
     # Raw taken from first 80 bytes of https://blockchain.info/block/624147?format=hex
