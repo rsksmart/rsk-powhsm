80dfac832f82238e6896d825a89624b403b7c13e
Ariel Mendelzon <amendelzon@rsk.co>
2026-07-27 15:32:12 -0300

    Fixed linting

--- firmware/src/hal/sgx/test/mock/mock_seal.c
+++ firmware/src/hal/sgx/test/mock/mock_seal.c
@@ -132,7 +132,8 @@ void assert_oe_seal_called_with(const void* plugin_id,
                                 size_t additional_data_size) {
     if (settings_count > 0) {
         assert(settings != NULL);
-        assert(memcmp(&G_oe_seal_args.settings, settings, sizeof(*settings)) == 0);
+        assert(memcmp(&G_oe_seal_args.settings, settings, sizeof(*settings)) ==
+               0);
     }
     assert(G_oe_seal_args.plugin_id == plugin_id);
     assert(memcmp(G_oe_seal_args.plaintext, plaintext, plaintext_size) == 0);
